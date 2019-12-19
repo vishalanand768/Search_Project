f6700278f8652351f7a4cce3d52c98f5605db493
vishalanand768 <vishalanand768@yahoo.com>
2019-12-19 21:48:23 +0530

    Add project files.

---
+++ searchdata.aspx
@@ -0,0 +1,80 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="searchdata.aspx.cs" Inherits="Search_Project.searchdata" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Name :</td>
+                    <td><asp:TextBox ID="txtname" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Age :</td>
+                    <td><asp:TextBox ID="txtage" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>City :</td>
+                    <td><asp:TextBox ID="txtcity" runat="server"></asp:TextBox></td>
+                </tr>
+
+
+                <tr>
+                    <td>Contact No. :</td>
+                    <td><asp:TextBox ID="txtcontact" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:Button ID="btnsave" runat="server" Text="Save" OnClick="btnsave_Click"></asp:Button></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td><asp:GridView ID="grd" runat="server" OnRowCommand="grd_RowCommand" AutoGenerateColumns="false">
+                      <Columns>
+
+                          <asp:TemplateField HeaderText="Name">
+                              <ItemTemplate>
+                                  <%#Eval("name") %>
+                              </ItemTemplate>
+                          </asp:TemplateField>
+
+                           <asp:TemplateField HeaderText="Age">
+                              <ItemTemplate>
+                                  <%#Eval("age") %>
+                              </ItemTemplate>
+                          </asp:TemplateField>
+
+                           <asp:TemplateField HeaderText="City">
+                              <ItemTemplate>
+                                  <%#Eval("city") %>
+                              </ItemTemplate>
+                          </asp:TemplateField>
+
+                           <asp:TemplateField HeaderText="Contact No.">
+                              <ItemTemplate>
+                                  <%#Eval("contact_no") %>
+                              </ItemTemplate>
+                          </asp:TemplateField>
+                        <asp:TemplateField HeaderText="Action">
+                        <ItemTemplate>
+                           <asp:Button ID="btndelete" runat="server" Text="Delete" CommandName="A" CommandArgument='<%#Eval("Id") %>' />
+                           <asp:Button ID="btnedit" runat="server" Text="Edit" CommandName="B" CommandArgument='<%#Eval("Id") %>' />
+                        </ItemTemplate>
+                        </asp:TemplateField>
+                    </Columns>
+                        </asp:GridView></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ searchdata.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace Search_Project
+{
+    public partial class searchdata : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["xyz"].ConnectionString);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindUser();
+            }
+        }
+
+        public void BindUser()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Usp_Student", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@action", 5);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            con.Close();
+
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+
+        protected void btnsave_Click(object sender, EventArgs e)
+        {
+            if (btnsave.Text == "Save")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Usp_Student", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", 1);
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@age", txtage.Text);
+                cmd.Parameters.AddWithValue("@city", txtcity.Text);
+                cmd.Parameters.AddWithValue("@contact_no", txtcontact.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindUser();
+            }
+            else if (btnsave.Text=="Update")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Usp_Student", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", 2);
+                cmd.Parameters.AddWithValue("@Id", ViewState["PP"]);
+                cmd.Parameters.AddWithValue("@name", txtname.Text);
+                cmd.Parameters.AddWithValue("@age", txtage.Text);
+                cmd.Parameters.AddWithValue("@city", txtcity.Text);
+                cmd.Parameters.AddWithValue("@contact_no", txtcontact.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindUser();
+                btnsave.Text = "Save";
+            }
+
+
+        }
+        protected void grd_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "A")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Usp_Student", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", 3);
+                cmd.Parameters.AddWithValue("@Id", e.CommandArgument);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                BindUser();
+            }
+            else if (e.CommandName == "B")
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("Usp_Student", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@action", 4);
+                cmd.Parameters.AddWithValue("@Id", e.CommandArgument);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                con.Close();
+                txtname.Text = dt.Rows[0][1].ToString();
+                txtage.Text = dt.Rows[0][2].ToString();
+                txtcity.Text = dt.Rows[0][3].ToString();
+                txtcontact.Text = dt.Rows[0][4].ToString();
+                btnsave.Text = "Update";
+                ViewState["pp"] = e.CommandArgument;
+            }
+        }
+
+    }
+}
